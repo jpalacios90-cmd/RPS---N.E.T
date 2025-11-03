dc9b9468ec0c03c3587065916586c09dd05bbe51
jpalacios90-cmd <jpalacios90@pimajted.net>
2025-11-03 16:47:06 -0700

    Squashed commit of the following:
    commit f62f6f676ce3361375887f0c6cd4a5db4e503824
    Author: eabakta80-byte <eabakta80@pimajted.net>
    Date:   Mon Oct 27 18:39:24 2025 -0700
    
        Correction player_choice
    
    commit 0cafd678b0c8071a0657a4fb073bceee107a14dc
    Author: eabakta80-byte <eabakta80@pimajted.net>
    Date:   Mon Oct 27 18:34:38 2025 -0700
    
        correction of the computer_choice
    
    commit 64c4e45ae057a10157161d10b6f2351e062d2eda
    Author: eabakta80-byte <eabakta80@pimajted.net>
    Date:   Mon Oct 27 18:29:33 2025 -0700
    
        final pre-draft
    
    commit 31149c1ebb4fe9593c77b151fdfb1f6e54f0acdc
    Author: eabakta80-byte <eabakta80@pimajted.net>
    Date:   Mon Oct 27 17:44:31 2025 -0700
    
        rectify
    
    commit 3baab042f336cf7e43c9d65285fe9bcc73880b33
    Author: eabakta80-byte <eabakta80@pimajted.net>
    Date:   Mon Oct 27 17:24:53 2025 -0700
    
        new game
    
        It is some change to the game

---
+++ Rock_Paper_Scissors_Player.py
@@ -0,0 +1,14 @@
+def player_info():
+    while True:
+        name = input("player name: ").strip()
+        # Ask for confirmation
+        confirm = input(f"Are you sure this is your name: '{name}'? (y/n): ").strip().lower()
+        if confirm in ("y", "yes"):
+            break
+        elif confirm in ("n", "no"):
+            print("Okay, let's try again.")
+        else:
+            print("Please answer 'y' or 'n'.")
+        print()
+
+    return {"name": name, "win": 0, "lose": 0,"drawn":0}
