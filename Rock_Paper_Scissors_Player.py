fb27b3f8008fb61909a198196aeabcfce906f217
eabakta80-byte <eabakta80@pimajted.net>
2025-11-05 18:40:38 -0700

    attempt of fixation of the save of the game

--- Rock_Paper_Scissors_Player.py
+++ Rock_Paper_Scissors_Player.py
@@ -14,21 +14,22 @@ def player_info():
     return {"name": name, "win": 0, "lose": 0,"drawn":0}
 
 
-
+def new_save(player):
+    with open("Player_info_Save.txt","w") as file: 
+        for element in player:
+            file.write("\n")       
+            file.write(f"{element}")
+        
 
 def save_game(player):
-    with open("Player_info_Save.txt","a") as file:        
+    with open("Player_info_Save.txt","a") as file: 
+        file.write("\n")       
         file.write(f"{player}")
-        file.write("\n")
-        # file.seek(1000)
-
-
+'''
     with open("Player_info_Save.txt","r") as file:
+
         redact=file.read()
-        return redact
-
-
-
+        return redact'''
 
 def t():
     files=save_game()
@@ -41,3 +42,57 @@ def t():
         print("gotcha")
     else:
         print("missing")
+
+
+def create_player_list():
+    new_list=[]
+    with open("Player_info_Save.txt","r") as file:
+        for line in file:
+            new_list.append(line)
+    return new_list
+
+
+new_list=create_player_list()
+
+def find_the_player(player,new_list):
+    for elements in new_list:
+        if player in elements:
+            print("c")
+            print(elements, new_list.index(elements))
+            saved_player_info=elements
+            return saved_player_info
+            
+        else:
+            print("d")
+    
+
+
+j=str(find_the_player("josh",new_list))
+
+
+l=str({'name': 'josh', 'win': 0, 'lose': 0, 'drawn': 0})
+k=str({'name': 'josh', 'win': 0, 'lose': 0, 'drawn': 0})
+
+
+print(type(j))
+
+h=str({'name': 'josh', 'win': 0, 'lose': 0, 'drawn': 0})
+print(h)
+
+if j==k:
+    print("g")
+else:
+    print("d")
+
+mj="{'name': 'josh', 'win': 0, 'lose': 0, 'drawn': 0}"
+
+if l==k:
+    print("Gsh")
+
+
+mj=h.removeprefix("{").removesuffix("}")
+# mj=mj.strip("{")
+
+
+print(mj)
+dict(mj)
